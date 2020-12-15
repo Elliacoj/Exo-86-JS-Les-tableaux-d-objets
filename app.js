eda6c3c28b073863e46818d4d573ea63275821e4
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-15 15:50:21 +0100

    Modif

--- app.js
+++ app.js
@@ -31,10 +31,22 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [citroen, peugeot, ford], // Vos voitures dans cette propriété !
 }
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
+
+for(let x = 0; x < voitures.collection.length; x++) {
+    voitures.collection[x].ouvrirPorte();
+
+}
+
+let span = document.createElement("span");
+document.getElementById("container").append(span);
+span.innerHTML =
+    "La peugeot est " + peugeot.color + "</br>" +
+    "La ford est " + ford.color  + "</br>" +
+    "La citroën est " + citroen.color;
 
 
